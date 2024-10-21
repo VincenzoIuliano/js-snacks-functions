f806ca195a4962e8670f42b5423e32896e4f99d2
VincenzoIuliano <bubucane@outlook.it>
2024-10-21 18:11:06 +0200

    ADD: ex-1 solution

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -6,8 +6,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 // Dichiara la funzione qui.
 
 
+function firstLetterArray(listNames) {
+    const letters = []
+   
+    for (let i = 0 ; i < names.length; i++) {
+        // console.log(listNames[i])
+        const name = listNames[i]
+        // console.log(name[0])
+        letters.push(name[0])
+    }
+
+    return letters
+}
+    
+
+
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstLetterArray(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
